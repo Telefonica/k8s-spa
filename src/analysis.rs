7fbdf34ad1ddc639d39fccc8c18cd770c05eef44
Ximo Guanter <joaquin.guantergonzalbez@telefonica.com>
2020-05-08 19:29:20 +0200

    improve analysis algorithm

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,22 +1,39 @@
-use super::{ContainerId, MemoryInfo};
-use log::info;
+use log::{debug, info};
+use rayon::prelude::*;
 use std::collections::HashMap;
+use crate::{ContainerId, MemoryInfo};
 
-pub fn calculate_requests(memory_info: &MemoryInfo, tolerance: f64) -> (u64, HashMap<&ContainerId, u64>) {
+pub fn calculate_requests(memory_info: &MemoryInfo, tolerance: f64) -> HashMap<&ContainerId, u64> {
   info!("Calculating requests...");
-  let total_request_size = memory_info.global.value_at_quantile(1.0 - tolerance);
-  let mut requests: HashMap<&ContainerId, u64> = HashMap::new();
-  for i in 1..=100 {
-    let total_request_at_percentile: u64 = memory_info.containers
-      .values()
-      .map(|h| h.value_at_percentile(i.into()))
-      .sum();
-    if total_request_at_percentile > total_request_size {
-      requests = memory_info.containers.iter().map(|(container_id, h)| {
-        (container_id, h.value_at_percentile((i - 1).into()))
-      }).collect();
-      break;
+  let MemoryInfo {
+    global: global_info,
+    containers: containers_info,
+    container_presence
+  } = memory_info;
+  let mut min: u8 = 0;
+  let mut max: u8 = 100;
+  while min < max {
+    debug!("min {}, max {}", min, max);
+    let percentile = (min + max) / 2;
+    let under_requested_count = global_info
+      .par_iter()
+      .filter(|v| {
+        let requests_at_ts: u64 = container_presence[&v.timestamp]
+          .iter()
+          .map(|id| containers_info[id].0.value_at_percentile(percentile as f64))
+          .sum();
+        requests_at_ts < v.value
+      }).count();
+    let risk = (under_requested_count as f64) / (global_info.len() as f64);
+    if risk > tolerance {
+      min = percentile + 1;
+    } else {
+      max = percentile;
     }
   };
-  (total_request_size, requests)
+  debug!("Percentile {}", min);
+  containers_info
+    .iter()
+    .map(|(id, histogram)| (id, histogram.0.value_at_percentile(min as f64)))
+    .collect()
 }
